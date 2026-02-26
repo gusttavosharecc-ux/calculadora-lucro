62ef74e36bc6e4d358cc1f80940012ec9eeed4d3
Seu Nome <seuemail@exemplo.com>
2026-02-26 12:44:37 -0300

    Adicionado SEO e botao de WhatsApp

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function calcular() {
         alert("Por favor, preencha os valores!");
     }
 }
+function compartilhar() {
+    const custo = document.getElementById('custo').value;
+    const venda = document.getElementById('venda').value;
+    const lucro = (venda - custo).toFixed(2);
+    const texto = `Usei a Calculadora de Lucro! 📈%0A*Custo:* R$ ${custo}%0A*Venda:* R$ ${venda}%0A*Lucro Real:* R$ ${lucro}%0A%0ACalcule o seu aqui: ${window.location.href}`;
+    window.open(`https://wa.me/?text=${texto}`, '_blank');
+}
